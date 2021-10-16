683d19a9f1f7cef85c44ddc2033e4fc14c8a6e17
Tianjiao <1585526224@qq.com>
2021-10-16 21:28:37 +0800

    推送到本地仓库啦

--- src/main/java/com/tian/GitTest.java
+++ src/main/java/com/tian/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello Hot-Fix Too!!!");
         System.out.println("Hello master 可能会造成合并冲突");
         System.out.println("Hello Hot-Fix 可能会造成合并冲突");
+        System.out.println("推送到远程仓库啦！！！");
     }
 }
