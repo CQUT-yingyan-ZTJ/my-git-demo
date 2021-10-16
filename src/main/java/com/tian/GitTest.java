e9cb67db3bb3caa5acdf31b9fee758c380ab2fc9
Tianjiao <1585526224@qq.com>
2021-10-16 15:51:56 +0800

    我再IDEA上面第一次提交代码

---
+++ src/main/java/com/tian/GitTest.java
@@ -0,0 +1,14 @@
+package com.tian;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ *
+ * @author Tianjiao
+ * @date 2021/10/16 15:42
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
